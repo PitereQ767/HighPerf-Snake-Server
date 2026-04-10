2206aa65ba4fc2945b784ae06ae3af1c26a608c1
Piotr <ratkowskipiotr04@gmail.com>
2026-04-10 23:58:37 +0200

    Added network protocol

---
+++ shared/include/Protocol.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstdint>
+
+namespace Protocol {
+    enum class MessageType : uint8_t {
+        JOIN_REQUEST = 0x01,
+        JOIN_ACCEPT = 0x02,
+        PLAYER_MOVE = 0x03,
+        SPAWN_FRUIT = 0x04,
+        PLAYER_DIE = 0x05
+    };
+
+    #pragma pack(push, 1)
+
+    struct MovePacket {
+        MessageType type;
+        uint16_t playerId;
+        int8_t dirX;
+        int8_t dirY;
+    };
+
+    struct FruitSpawnPacket {
+        MessageType type;
+        uint16_t x;
+        uint16_t y;
+    };
+
+    #pragma pack(pop)
+}
